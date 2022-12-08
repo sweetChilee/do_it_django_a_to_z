86d16544abe7e113ec669dffec8af55367699c36
sweetChilee <rhkdrb99@GGLEE.local>
2022-12-08 11:53:32 +0900

    Nginx 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
         'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.environ.get('SQL_USER', 'user'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'USER': os.environ.get('SQL_USER', 'pomeranian'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'zmffpqjfl!@2a'),
         'HOST': os.environ.get('SQL_HOST', 'localhost'),
         'PORT': os.environ.get("SQL_PORT", '5432'),
     }
